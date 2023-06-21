0549a796d794f99ef8f8a9d99b64427da1372c79
Jamma Beans <admin@jammabeans.com>
2023-06-21 16:35:30 -0600

    Modify work tracking, user gas refund function

--- contracts/VMPX.sol
+++ contracts/VMPX.sol
@@ -4,24 +4,22 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract VMPX is ERC20("VMPX", "VMPX"), ERC20Capped(108_624_000 ether) {
-
+contract VMPX is ERC20("VMPX", "VMPX"), ERC20Capped(108_624_000_000 ether) {
     string public constant AUTHORS = "@MrJackLevin @ackebom @lbelyaev faircrypto.org";
 
     uint256 public constant BATCH = 200 ether;
-    uint256 public immutable cycles; // depends on a network block side, set in constructor
+    uint256 public immutable cycles; 
 
-    uint256 public counter;
-    mapping(uint256 => bool) private _work;
+    mapping(address => uint256[]) private _work;
 
     constructor(uint256 cycles_) {
         require(cycles_ > 0, 'bad limit');
         cycles = cycles_;
     }
 
-    function _doWork(uint256 power) internal {
+    function _doWork(address account, uint256 power) internal {
         for(uint i = 0; i < cycles * power; i++) {
-            _work[++counter] = true;
+            _work[account].push(1);
         }
     }
 
@@ -30,9 +28,14 @@ contract VMPX is ERC20("VMPX", "VMPX"), ERC20Capped(108_624_000 ether) {
     }
 
     function mint(uint256 power) external {
-        require(power > 0 && power < 196, 'power out of bounds');
+        require(power > 0, 'power has to be positive');
         require(tx.origin == msg.sender, 'only EOAs allowed');
-        _doWork(power);
+        require(totalSupply() + (BATCH * power) <= cap(), "minting would exceed cap");
+        _doWork(msg.sender, power);
         _mint(msg.sender, BATCH * power);
+    }
+
+    function clearWork() external {
+        delete _work[msg.sender];
     }
 }
